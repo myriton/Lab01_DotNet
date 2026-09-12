fb308738770094c98765212479b1873ce91560d4
myriton <nikasevcenko642@gmail.com>
2026-09-12 11:05:33 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет от ИСП-242!");
+Console.WriteLine("Автор: Глазков Александр Шевченко Вероника");
+Console.WriteLine("Год: 2026");
